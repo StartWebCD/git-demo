ff4ced6dd02acc79e8342bf59b1f84eb14d65cc6
StartWebCD <mail4wdnet@gmail.com>
2022-09-23 15:48:14 +0300

    Am adaugat River

---
+++ River/River.js
@@ -0,0 +1,24 @@
+  function myFunction() {
+  var x = document.getElementById("inforau");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+    function myFunction2() {
+  var y = document.getElementById("inforau2");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+    function myFunction3() {
+  var z = document.getElementById("inforau3");
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "block";
+  }
+}
